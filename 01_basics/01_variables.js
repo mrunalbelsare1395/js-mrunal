3825c7a5f604f62ecfca1d3643022a1efece22ac
mrunalbelsare1395 <mrunalbelsare13@gmail.com>
2024-05-07 11:10:44 +0000

    commiting var and datatype file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accId = 123;
+let accName = "Mrunal";
+let accEmail = "mrunalbelsare@gmail.com";
+//should not use var due to functional scope  and block scope issues 
+
+console.table([accId, accName, accEmail]);
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,2 @@
+// alert(3+3) //invalid as we are using node js and not browser
+

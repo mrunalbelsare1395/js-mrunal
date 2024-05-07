df8c2c05a11a06a5e146dd2886a56e68a3ff58b9
mrunalbelsare1395 <mrunalbelsare13@gmail.com>
2024-05-07 11:46:13 +0000

    conversion and operations

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,26 @@
+let score = "33";
+let valueOfNumber = Number(score);
+console.log(typeof valueOfNumber);
+
+let scoreSecond = "33abc";
+let valueOfNumberSecond = Number(scoreSecond);
+console.log(typeof valueOfNumberSecond);
+
+
+//"33" = 33
+// "33 abc " = NaN;
+// true = 1
+// false =0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 - true
+//0 - false 
+
+
+let someNo = 33
+let stringNo = String(someNo);
+console.log(stringNo);
+console.log(typeof stringNo)

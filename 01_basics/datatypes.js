1e3e15170066126cdf4c4882482c5550446bee5e
mrunalbelsare1395 <mrunalbelsare13@gmail.com>
2024-05-07 11:28:18 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,2 +1,12 @@
 // alert(3+3) //invalid as we are using node js and not browser
 
+// number 
+// string 
+// boolean 
+// null
+// undefined
+// bigint 
+// symbol
+
+console.log(typeof null); //o/p is object
+console.log(typeof undefined); //o/p is undefined

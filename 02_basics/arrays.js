abcb815f562f6daa99878b7edb55a1eec895c833
mrunalbelsare1395 <mrunalbelsare13@gmail.com>
2024-05-10 16:23:41 +0000

    arrays

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -1,1 +1,33 @@
-//arrays
+// //arrays
+
+// const myArr = [0,1,2,3,4];
+// // console.log(myArray[3]);
+
+// // myArray.push(5)
+// // console.log(myArray)
+
+// // myArray.pop()
+// // console.log(myArray)
+
+// // myArray.unshift(9)
+// // console.log(myArray)
+
+// // myArray.shift()
+// // console.log(myArray)
+
+// // myArray.includes(67)
+// // console.log(myArray)
+ 
+// const newArr = myArr.join();
+// console.log(myArr);
+
+const myArr2 = [6,7,8,9]
+//console.log(myArr2);
+
+const myNewArray1 = myArr2.slice(1,3)
+console.log("OG Array" , myArr2);
+console.log("sliced array" , myNewArray1);
+
+const myNewArray2 = myArr2.splice(1,3)
+console.log("OG Array1" , myArr2);
+console.log("spliced array" , myNewArray2);

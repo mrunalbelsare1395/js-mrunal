444fe430a99745a3a91a201ee5ace5b90a6d68d3
mrunalbelsare1395 <mrunalbelsare13@gmail.com>
2024-05-10 18:59:22 +0000

    arrays2

---
+++ 02_basics/arrays2.js
@@ -0,0 +1,11 @@
+// const myArr1 = ['abc' , 'xyz'];
+// const myArr2 = ['klm' , 'qrs'];
+// const mySpreadedArray = [... myArr1,...myArr2 ]
+// console.log(mySpreadedArray);
+
+
+const anotherArray = [1,2, [3,4], 5,[6,[7,8]]]
+const newArr = anotherArray.flat(Infinity)
+console.log(newArr)
+
+

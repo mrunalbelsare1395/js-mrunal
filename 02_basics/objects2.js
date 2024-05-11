a14c08585c8189a044da81a9c3a2c71e50cbe734
mrunalbelsare1395 <mrunalbelsare13@gmail.com>
2024-05-11 10:23:42 +0000

    objects2

---
+++ 02_basics/objects2.js
@@ -0,0 +1,53 @@
+//objects inside objects
+
+// const tinderUser = {};
+
+// tinderUser.id = '4',
+// tinderUser.name = "mrunal",
+// tinderUser.isLoggedIn = true
+
+// //console.log(tinderUser);
+
+// const anotherUser = {
+//     name : "mru" ,
+//     fullName : {
+//         lastName : "belsare",
+
+//     }
+// }
+// console.log(anotherUser.fullName.lastName)
+
+
+// //combining objects
+// const obj1 = {1 : "a", 2: "b"}
+// const obj2 = {3 : "c", 4: "d"}
+
+// //const obj3 = Object.assign({} , obj1 , obj2) //method1
+// const obj3 = {...obj1,... obj2} //method2
+// console.log(obj3);
+
+//objects inside arrays - we get when we access data drom DATABASE
+
+// const obj7 = [
+//     {
+//         id :1,
+//         name : "mru"
+//     },
+//     {
+//         email: "abc@gmail.com"
+//     }
+// ]
+
+// console.log(obj7[0].name)
+
+//accessing keys and values from the objects
+const obj = {
+    id: 1,
+    name: "Mrunal",
+    email : "abc@gmail.com"
+}
+
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+console.log(obj.hasOwnProperty('email'))

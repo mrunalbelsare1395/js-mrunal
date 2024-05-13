d5bdf6548f5e3b61ea8ad504fc585287c23eae09
mrunalbelsare1395 <mrunalbelsare13@gmail.com>
2024-05-13 15:06:50 +0000

    iife

---
+++ 03_basics/iife.js
@@ -0,0 +1,11 @@
+// (function chai() {
+//     console.log(`data base coonected`);
+// }) () ;
+
+// (() => {
+//     console.log(`Database2 Connected`)
+// }) ()
+
+((name) => {
+    console.log(`Hey ${name}`)
+}) ('Mrunal');

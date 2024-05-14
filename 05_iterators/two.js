1a86d6f5a6ff5d4f6b3297af931eb6ea1ce75e0a
mrunalbelsare1395 <mrunalbelsare13@gmail.com>
2024-05-14 15:15:44 +0000

    iterators two

---
+++ 05_iterators/two.js
@@ -0,0 +1,23 @@
+//while loop
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`value of i is ${i}`)
+//     i = i + 2;
+// }
+
+//array in while loop
+// let myArray = ['abc', 'xyz','lmn']
+// let i = 0; //since index of an array starts from 0
+// while(i < myArray.length) { 
+//     console.log(`value of array is ${myArray[i]}`);
+//     i = i+1;
+
+// }
+
+//DO-WHILE LOOP
+
+let score = 1 
+do {
+    console.log(`value of score is ${score}`);
+    score++;
+} while ( score <= 10);
